0f7bf8b903d522ce409654c49f408943025ab668
matias.escudero.b <matias.escudero.b@usach.cl>
2023-04-08 13:56:32 -0400

    commit inicial

---
+++ labs/lab-1_analisis_estadistico/lab-1_analisis_estadistico.R
@@ -0,0 +1,29 @@
+# Minería de datos
+# Profesor: Max Chacón
+# Alumnos: Matías Escudero, Joaquín Macías
+# Doctorado en Ciencias de la Ingeniería con Mención en Informática - USACH
+
+### LIBRERÍAS ###
+library(readr)
+library(tidyverse)
+
+### ENTRADAS ###
+
+## Contenido del DataFrame
+
+allhyper_df = read_csv("data/allhyper.data",
+                    col_names = FALSE)
+
+
+## Se extraen los nombres de las columnas extraídas a partir del archivo allhyper.names
+allhyper_names = read_lines("data/allhyper.names")
+
+column_names = gsub(":.*", "", contenido_archivo[11:39])
+column_names = gsub(" ", "_", column_names)
+column_names = c(column_names, "class") # Se añade la columna "class" que no se especifica en el archivo
+
+## Se asignan al DF
+colnames(allhyper_df) = column_names
+
+
+
