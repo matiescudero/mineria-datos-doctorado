e1d543ec0520caf4fb3b63e7ad543fcae834c2fa
matias.escudero.b <matias.escudero.b@usach.cl>
2023-04-16 20:15:22 -0400

    se leen los ? como NA

--- labs/lab-1_analisis_estadistico/lab-1_analisis_estadistico.R
+++ labs/lab-1_analisis_estadistico/lab-1_analisis_estadistico.R
@@ -9,22 +9,37 @@ library(tidyverse)
 
 #### ENTRADAS ####
 
-## Contenido del DataFrame
+## Se lee el contenido del DataFrame, diciendole que "?" representa los NA's
 
 allhyper_df = read_csv("data/allhyper.data",
-                       col_names = FALSE)
+                       col_names = FALSE,
+                       na = "?")
 
 
-## Se extraen los nombres de las columnas extraídas a partir del archivo allhyper.names
+## Se extraen los nombres de las columnas partir del archivo allhyper.names
+
+# Abre el archivo
 allhyper_names = read_lines("data/allhyper.names")
 
+# Extrae únicamente los nombres, descartando la información que no es relevante.
 column_names = gsub(":.*", "", contenido_archivo[11:39])
 column_names = gsub(" ", "_", column_names)
 column_names = c(column_names, "class") # Se añade la columna "class" que no se especifica en el archivo
 
-## Se asignan al DF
+## Se asignan las columnas al DF
 colnames(allhyper_df) = column_names
 
 
 #### PRE-PROCESAMIENTO ####
+
+# Se generan nuevas columnas que separe la clase en nombre e identificador
+
+allhyper_df = extract(data = allhyper_df, 
+                      col = class, 
+                      into = c("class_name", "class_id"), 
+                      regex = "(.*)\\|(.*)")
+
+# Se elimina class_id, ya que no entrega información relevante
+allhyper_df$class_id = NULL
+
 
